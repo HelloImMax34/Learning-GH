981ccb79a85cd22f9ed1796e653e492b68e627aa
Max Goldman <maxwellgoldman34@gmail.com>
2023-01-11 11:30:46 -0800

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by Max Goldman on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
